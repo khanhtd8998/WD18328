c700c13c7ca79facd19dcf26c49cad661b1caf12
khanhtd8998 <trankhanhhy199x@gmail.com>
2024-06-26 17:06:50 +0700

    feat: todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [data, setData] = useState('');
+  const [datas, setDatas] = useState<{ text: string, checked: boolean }[]>([]);
+  const handleAdd = () => {
+    setDatas([...datas, { text: data, checked: false }]);
+    setData('');
+  }
+  console.log(datas);
+  const handleDelete = (index: number) => {
+    const newDatas = datas.filter((_, i) => i !== index);
+    // const newDatas = [...datas];
+    // newDatas.splice(index, 1);
+    setDatas(newDatas);
+  }
+  const handleCheck = (index: number) => {
+    const newDatas = datas.map((item, i) => {
+      if (i === index) {
+        return { ...item, checked: !item.checked };
+      }
+      return item
+    });
+    setDatas(newDatas);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="mt-32">
+        <div className="px-4 sm:px-8 max-w-5xl m-auto">
+          <h1 className="text-center font-semibold text-xl">Todo List</h1>
+          <input type="text" value={data} onChange={e => setData(e.target.value)} className="w-full border border-gray-200 rounded px-4 py-2" />
+          <button onClick={handleAdd} type="button" className="focus:outline-none mt-2 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add</button>
+          <ul className="border border-gray-200 rounded overflow-hidden shadow-md">
+            {
+              datas.map((item, index) => <li key={index} className="py-2 px-4">
+                <div className='flex justify-between'>
+                  <div className="flex items-center">
+                    <input className='mr-2' type="checkbox" checked={item.checked} onChange={() => handleCheck(index)} />
+                    <p className={item.checked ? 'line-through text-lg' : 'text-lg'}>{index + 1}-{item.text}</p>
+                  </div>
+                  {item.checked && (
+                    <button
+                      onClick={() => handleDelete(index)}
+                      type="button"
+                      className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-0.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                    >
+                      X
+                    </button>
+                  )}
+                </div>
+              </li>)
+            }
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
+
 }
 
 export default App
