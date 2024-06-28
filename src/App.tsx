c8f771c4f7cd4abf14c4dc465670ac8d522d9d92
khanhtd8998 <trankhanhhy199x@gmail.com>
2024-06-28 22:43:26 +0700

    feat: crud product in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,158 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { useForm } from 'react-hook-form';
+
+interface Product {
+  id: number;
+  name?: string;
+  description?: string;
+  checked: boolean;
+}
 
 function App() {
-  const [data, setData] = useState('');
-  const [datas, setDatas] = useState<{ text: string, checked: boolean }[]>([]);
-  const handleAdd = () => {
-    setDatas([...datas, { text: data, checked: false }]);
-    setData('');
+  const [products, setProducts] = useState<Product[]>([]);
+  const [currentProduct, setCurrentProduct] = useState<Product>({} as Product);
+  const { reset, register, handleSubmit, formState: { }, } = useForm();
+
+  useEffect(() => {
+    fetch('http://localhost:3000/products')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const handleDelete = (id: number) => {
+    if (confirm('Are you sure you want to delete this product?')) {
+      fetch(`http://localhost:3000/products/${id}`, {
+        method: 'DELETE',
+      })
+        .then((res) => res.json())
+        .then(() => setProducts(products.filter((p) => p.id !== id)));
+    }
+  };
+
+
+
+  const onEdit = async (data: any) => {
+    try {
+      fetch(`http://localhost:3000/products/${currentProduct.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ...currentProduct, name: data.name2, description: data.description2 }),
+      })
+        .then(res => res.json())
+        .then(updatedProduct => {
+          setProducts(products.map((item) => (item.id === updatedProduct.id ? updatedProduct : item)));
+          setCurrentProduct({} as Product);
+          reset();
+        })
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onAdd = (data: any) => {
+    //Nếu nhập dữ liệu xong chưa ấn thêm và ấn sửa sp nào đó, xong tiếp đó mới ấn thêm mới
+    const { name2, description2, ...newData } = data
+    fetch('http://localhost:3000/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ...newData, checked: false }),
+    })
+      .then((res) => res.json())
+      .then((newProduct) => {
+        setProducts([...products, newProduct]);
+        reset();
+      });
   }
-  console.log(datas);
-  const handleDelete = (index: number) => {
-    const newDatas = datas.filter((_, i) => i !== index);
-    // const newDatas = [...datas];
-    // newDatas.splice(index, 1);
-    setDatas(newDatas);
-  }
-  const handleCheck = (index: number) => {
-    const newDatas = datas.map((item, i) => {
-      if (i === index) {
-        return { ...item, checked: !item.checked };
-      }
-      return item
-    });
-    setDatas(newDatas);
+
+  const handleChangeChecked = async (data: Product) => {
+    try {
+      const res = await fetch(`http://localhost:3000/products/${data.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ...data, checked: !data.checked }),
+      })
+      if (res.status != 200) return alert('Cập nhật checked thất bại')
+      const result = await res.json()
+      setProducts(products.map((item) => (item.id === result.id ? result : item)));
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <>
-      <div className="mt-32">
-        <div className="px-4 sm:px-8 max-w-5xl m-auto">
-          <h1 className="text-center font-semibold text-xl">Todo List</h1>
-          <input type="text" value={data} onChange={e => setData(e.target.value)} className="w-full border border-gray-200 rounded px-4 py-2" />
-          <button onClick={handleAdd} type="button" className="focus:outline-none mt-2 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add</button>
-          <ul className="border border-gray-200 rounded overflow-hidden shadow-md">
-            {
-              datas.map((item, index) => <li key={index} className="py-2 px-4">
-                <div className='flex justify-between'>
-                  <div className="flex items-center">
-                    <input className='mr-2' type="checkbox" checked={item.checked} onChange={() => handleCheck(index)} />
-                    <p className={item.checked ? 'line-through text-lg' : 'text-lg'}>{index + 1}-{item.text}</p>
-                  </div>
-                  {item.checked && (
-                    <button
-                      onClick={() => handleDelete(index)}
-                      type="button"
-                      className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-0.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                    >
-                      X
-                    </button>
-                  )}
-                </div>
-              </li>)
-            }
-          </ul>
+      <div className='container'>
+        <div className='flex justify-center'>
+          <form className="mt-3" onSubmit={handleSubmit(onAdd)}>
+            <div className="mb-5">
+              <input placeholder='Name' className="border p-1 rounded-sm" {...register('name')} /> <br />
+            </div>
+            <div className="mb-5">
+              <input placeholder='Description' className="border p-1 rounded-sm" {...register('description')} /> <br />
+            </div>
+            <button
+              className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-md text-sm px-5 py-1.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900"
+              type="submit"
+            >
+              Add
+            </button>
+          </form>
         </div>
+        <h1 className="container my-3 text-3xl text-center">List Product</h1>
+        {products.map((item, index) => (
+          <div key={item.id} className="flex justify-center container mx-auto">
+            {(item.id === currentProduct.id && currentProduct.checked === false) ? (
+              <form className="flex items-center" onSubmit={handleSubmit(onEdit)}>
+                <input className="border p-1 rounded-sm" type="text" {...register('name2')} />
+                <input className="border p-1 rounded-sm" type="text" {...register('description2')} />
+                <button
+                  className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900"
+                  type="submit"
+                >
+                  OK
+                </button>
+                <button
+                  onClick={() => setCurrentProduct({} as Product)}
+                  className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-1.5 ml-1 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'
+                  type="submit"
+                >
+                  X
+                </button>
+              </form>
+            ) : (
+              <div className="flex items-center">
+                <input className="mr-2" type="checkbox" onClick={() => { handleChangeChecked(item) }} />
+                <p onClick={() => {
+                  setCurrentProduct(item)
+                  reset({
+                    name2: item.name,
+                    description2: item.description
+                  })
+                }}
+                  className={item.checked ? 'text-lg line-through w-96' : 'text-lg w-96 hover:cursor-pointer'}>
+                  {index + 1} - {item.name} - {item.description}
+                </p>
+                <button
+                  className={'focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-0.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'}
+                  onClick={() => handleDelete(item.id)}
+                  type="button"
+                >
+                  Delete
+                </button>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
-    </>
-  )
 
+    </>
+  );
 }
 
-export default App
+export default App;
